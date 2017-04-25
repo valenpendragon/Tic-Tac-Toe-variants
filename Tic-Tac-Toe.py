ae18d8111b38e54f12efd3f7b501e3d6e85ed0c0
Jeffrey Scott <valenpendragon@gmail.com>
2017-04-25 16:02:51 -0400

    Fixed several error in cycling moves by the players. Added line to remove coordinates when a player chooses and empty space.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -25,3 +25,48 @@ print "Thank you. Confirming that the second player is "+player2
 
 print player1+' and '+player2+', I am initializing the game for you.'
 print "This will just take a moment."
+
+# Initializing player_moves. Note, they are empty sets.
+player_moves = {}
+player_moves[player1] = set()
+player_moves[player2] = set()
+
+# We also need the number of players.
+num_players = len(players)
+
+# Next, we need to initialize the board_moves set. As a player takes a cell.
+# it will be removed from board_moves and added to their moves set in the 
+# player_moves dictionary. This set is composed of coordinates corresponding
+# to the coordinate blocks printed in each game cell.
+
+board_moves = init_board(size)
+
+# player_turn tracks who is the next player to be asked for a move.
+player_turn = player1
+move_count = 0
+for num in xrange(0, size**2):  # This is enough moves to fill the board.
+    # Print the board based on current moves
+    print_board(player_moves, size)
+    print player_turn+", it is your turn. I will prompt you for row and column."
+    print "Use single digits less than "+str(size)+". I will do the rest."
+    row = raw_input("Which row?")
+    row = int(row)
+    if ((row in range(0,size)) != True ):
+        print "Please try again. The row was invalid."
+        continue
+    col = raw_input("Which col?")
+    col = int(col)
+        if ((col in range(0,size)) != True ):
+        print "({},{}) is not available. Please try again.".format(row,col)
+        print "Available spaces: ", board_moves
+        continue
+    if ((row,col) in board_moves) != True:
+        # That cell is occupied already.
+        print "That move is not available. Please try again."
+        continue
+    print "("+str(row)+","+str(col)+") is available."
+    player_moves[player_turn].add((row,col))
+    board_moves.discard((row,col))
+    move_count += 1
+    move_count %= num_players
+    player_turn = players[move_count]    
