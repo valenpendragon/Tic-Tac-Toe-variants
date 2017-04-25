29ed586dea5242393efa6366f3de437f68797c2f
Jeffrey Scott <valenpendragon@gmail.com>
2017-04-25 05:47:31 -0400

    Changed top_bottom to horiz_border. Coord block prints perfectly.

--- start_screen.py
+++ start_screen.py
@@ -11,14 +11,15 @@ def start_screen(size=3):
     border_char = 'B'
     # reference_ruler (horiz and vert) allows us to check lengths and positions.
     reference_rule_horiz = '0123456789'*(size - 1)
-    top_bottom_border = border_char*((5*size) + 1)
+    horiz_border = border_char*((5*size) + 1)
     print reference_rule_horiz
+    print horiz_border
     # Next, we need to create and print coordinate blocks.
-    coord_block = border_char
     for row in xrange(0,1):      # For now, this is just row 0.
+        coord_block = border_char
         for col in xrange(0,size):
             new_block = "(" + str(row)+"," + str(col) + ")" + border_char
             coord_block = coord_block + new_block
-    print top_bottom_border
-    print coord_block
+        print horiz_border
+        print coord_block
     return
