9e8800c77e5515c32b10015ef5354390a967a63b
Jeffrey Scott <valenpendragon@gmail.com>
2017-04-26 15:20:31 -0400

    Moved wins initialization to a function called create_wins.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -34,48 +34,18 @@ player_moves[player2] = set()
 # We also need the number of players.
 num_players = len(players)
 
-# Now, we need to create a dictionary of winning moves. This is going to require three
-# levels of nesting. The outermost layer is type of win. These key values are: column,
-# row, or diagonal. diagonal has only two elements until a smaller win size is
+# Now, we need to create a dictionary of winning moves. This dictionary has three
+# levels of nesting. The outermost layer is "type of win". The key values are: columns,
+# rows, and diagonals. diagonals has only two elements until a smaller "win size" is
 # implemented. Columns connects to a nested dictionary with "size" number of keys.
-# {column: {0 : set(coordinates of column0), 1 : set(coordinates of column1), ...}}
+# {columns: {0 : set(coordinates of column0), 1 : set(coordinates of column1), ...}}
 # These integer keys then map to a set of coordinates for each column0, ...columnn.
-# rows has the same kind double nesting. Diagonal has two keys, ['down', 'up'] and 
+# rows has the same kind double nesting. Diagonals has two keys, ['down', 'up'] and 
 # they, in turn point to their contents. {down: set(coordinates of the down diagonal),
 # up : set (coordinates of the up diagonal)}.
+# wins is initialized by the function below:
 
-wins = {}                 # Initialize the master dictionary.
-wins['columns'] = {}      # Initialize the columns dictionary.
-wins['rows'] = {}         # Initialize the rows dictionary.
-wins['diagonals'] = {}    # Initialize the diagonals dictionary.
-
-# print wins
-# There are only two diagonal, up and down. So, we will initialize them as empty sets.
-# We will initialize them at the innermost for loop below.
-wins['diagonals']['down'] = set()
-wins['diagonals']['up'] = set()
-# print wins
-
-# This for loop will iterate the rows and columns nested dictionaries to initialize
-# the sets at the second nested level.
-for i in xrange(0, size):
-    wins['columns'][i] = set()
-    wins['rows'][i] = set()
-    # The down diagonal has ascending rows and columns. The tough one is the up 
-    # or ascending diagonal. The columns ascend as the rows descend. Performing
-    # this inside the nested loop would make it repeat the steps, without adding
-    # anything (sets are unique elements).
-    wins['diagonals']['down'].add((i,i))          # both ascend in value
-    wins['diagonals']['up'].add((size - i -1,i))     # cols ascend while rows descend
-    
-    # Now, we focus on adding the size number of cell coordinates that make up row, 
-    # column, and diagonal wins. They will addressed via the next nested loop.
-    for j in xrange(0, size):
-        wins['columns'][i].add((j,i))  # Here, j is ascending rows on column i.
-        wins['rows'][i].add((i,j))     # Here, j is the right moving column in row i.
-# print wins['columns']
-# print wins['rows']
-# print wins['diagonals']
+wins = create_wins(size)
 
 # Next, we need to initialize the board_moves set. As a player takes a cell.
 # it will be removed from board_moves and added to their moves set in the 
